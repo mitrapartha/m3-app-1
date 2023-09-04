8ea5cbd76b9d4d36dfd4b9c4150b03415b359f25
mitra.partha@gmail.com <mitra.partha@gmail.com>
2023-09-04 09:07:15 +0000

    Created ServerModule1; Edited Form1; Created Choose Project field

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -10,3 +11,18 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def drop_down_1_show(self, **event_args):
+    """This method is called when the DropDown is shown on the screen"""
+    pass
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
+  def drop_down_2_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
+
+
